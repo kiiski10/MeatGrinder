e7958335930f44eafd2e66cbe02cfa0591169db5
kiiski10 <pekka@levonoja.fi>
2026-04-03 19:24:27 +0300

    Dont allow multiple belt sections in same grid position

--- components/mouse_cursor.gd
+++ components/mouse_cursor.gd
@@ -3,7 +3,7 @@ class_name MouseCursor extends Area2D
 
 @onready var input: InputComponent = $InputComponent
 @export var arena: Arena
-
+var grid_size: int = 16
 
 func _process(_delta: float) -> void:
 	var mouse_on_arena: bool = input.mouse_position.x < arena.factory.position.x
@@ -11,8 +11,8 @@ func _process(_delta: float) -> void:
 		position = input.mouse_position
 	else:
 		# Mouse click on factory snaps to grid
-		var grid_position: Vector2 = (input.mouse_position - arena.factory.position) / 16
-		position = arena.factory.position + round(grid_position) * 16
+		var grid_position: Vector2 = (input.mouse_position - arena.factory.position) / grid_size
+		position = arena.factory.position + round(grid_position) * grid_size
 
 	if input.mouse_click:
 		if mouse_on_arena:
@@ -24,7 +24,15 @@ func _process(_delta: float) -> void:
 			)
 		else:
 			print("Factory click at: ", input.mouse_position)
-			arena.factory.set_item(arena.factory.conveyor_belt_scene, position)
+			# check if there is already an item at the clicked position
+			var position_taken: bool = false
+			for i in arena.factory.conveyor_belt_sections:
+				if i.global_position.distance_to(global_position) < grid_size:
+					print("Item already at position: ", position)
+					position_taken = true
+					break
+			if not position_taken:
+				arena.factory.set_item(arena.factory.conveyor_belt_scene, position)
 
 
 func _ready() -> void:
